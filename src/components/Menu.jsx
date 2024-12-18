4e1be34502b88059bb2cd1c595cde1892de090a1
Ameya Joshi <ameyasureshjoshi@gmail.com>
2024-12-18 16:22:27 -0600

    Got sliding menu working with keyboard and mouse
    Menu is also responsive

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,19 +1,57 @@
+import { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router';
 
 const Menu = ({ home, portfolio, experience, outsideWork }) => {
+  const [isNavOpen, setIsNavopen] = useState(false);
+
+  const handleNavOnClick = () => {
+    const navigation = document.getElementById('slidingNav');
+
+    navigation.classList.toggle('nav-visible');
+    setIsNavopen(!isNavOpen);
+  };
+
+  const handleOnKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      handleNavOnClick(e);
+    }
+  };
+
   return (
     <section className='menu-wrapper'>
-      <section className='menu-icon'>
+      <section
+        className='menu-icon'
+        id='menuIcon'
+        onClick={handleNavOnClick}
+        onKeyDown={handleOnKeyDown}
+        tabIndex={0}
+      >
         <div className='bar'></div>
         <div className='bar'></div>
         <div className='bar'></div>
       </section>
 
-      <nav className='drop-down-nav'>
-        {home && <Link to='/'>Landing Page</Link>}
-        {portfolio && <Link to='/portfolio'>Portfolio</Link>}
-        {experience && <Link to='/experience'>Career Experience</Link>}
-        {outsideWork && <Link to='/outside-work'>Hobbies</Link>}
+      <nav className='drop-down-nav' id='slidingNav'>
+        {home && (
+          <Link to='/' tabIndex={isNavOpen ? 0 : -1}>
+            Landing Page
+          </Link>
+        )}
+        {portfolio && (
+          <Link to='/portfolio' tabIndex={isNavOpen ? 0 : -1}>
+            Portfolio
+          </Link>
+        )}
+        {experience && (
+          <Link to='/experience' tabIndex={isNavOpen ? 0 : -1}>
+            Career Experience
+          </Link>
+        )}
+        {outsideWork && (
+          <Link to='/outside-work' tabIndex={isNavOpen ? 0 : -1}>
+            Hobbies
+          </Link>
+        )}
       </nav>
     </section>
   );
--- src/pages/MainContent.jsx
+++ src/pages/MainContent.jsx
@@ -20,10 +20,10 @@ const Main = () => {
         <p className='main-content-wrapper-p'>
           I'm <b>Ameya Joshi&nbsp;</b>, a Frontend/React developer based in the
           Oklahoma City metro area, with 5+ years of professional experience
-          that includes <b>React</b> and <b>TypeScript</b>. I enjoy coding, am
-          an avid aviation enthusiast and have a Meteorology degree as well. I
-          call it the "three-way cloud connection". In keeping with that theme
-          are some aircraft-style windows below.&nbsp;
+          that includes <b>React</b> and <b>TypeScript</b>. I enjoy web
+          development, am an avid aviation enthusiast and have a Meteorology
+          degree as well. I call it the "three-way cloud connection". In keeping
+          with that theme are some aircraft-style windows below.&nbsp;
           <span className='mobile-only'>Tap&nbsp;</span>
           <span className='desktop-only'>Open&nbsp;</span>each window's shade to
           have a look at my resume, career experience and what I like to do
